f0dbf07ef69a878fc1afc15a0fa697f278a8a46b
Tomas Valent <equivalent@eq8.eu>
2020-10-26 17:07:57 +0100

    check if town.name present

--- lib/slovakia_postcodes.rb
+++ lib/slovakia_postcodes.rb
@@ -9,6 +9,10 @@ module SlovakiaPostcodes
     locations = File.read('./postalcodecoordinates_sk.json')
     locations = JSON.parse(locations)
 
+    ## If you need to debug run
+    #
+    #    binding.irb
+
     names.each do |town_name, data|
       data["postalCodes"].each do |postcode|
 
@@ -21,11 +25,19 @@ module SlovakiaPostcodes
       end
     end
 
-
-    ## If you need to debug run
-    #
-    #    binding.irb
-
     File.write('./generated.json', JSON.pretty_generate(locations))
+    puts "Ok file 'generated.json' generated successfully"
+    puts "\n\n"
+    puts "Now I'm running check if there are any postcodes without town.name:"
+
+    no_problem = true
+    locations.each do |postcode, data|
+      if data['town'].nil? || data['town']['name'].nil?
+        no_problem = false
+        puts "Postcode #{postcode} has no town.name!"
+      end
+    end
+
+    puts "All Ok! No postcodes without town.name " if no_problem
   end
 end
